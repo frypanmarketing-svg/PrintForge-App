b8d620ab2323ed1c9bc7a396c674ccea91716b46
Yasir <yasirk2026@yahoo.com>
2026-01-30 20:25:59 +0500

    Initial commit

---
+++ app/components/Header.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+
+export default function Header() {
+    return (
+        <>
+            <header>
+                <img src="/printforge-logo.png"></img>
+
+                <nav>
+                    <ul>
+                        <li>3D MODELS</li>
+                        <li>ABOUT</li>
+                    </ul>
+                </nav>
+
+            </header>
+        </>
+    )
+}
---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,18 @@
+export default function HeroSection() {
+    return(
+        <div>
+
+          <div>
+            <h1>Your go-to platform for 3D printing files</h1>
+            <h2>Discover what’s possible with 3D printing</h2>
+            <p className="subheading">Join our community of creators and explore a vast library of user-submitted models.</p>
+            <button className="hero-cta">BROWSE MODELS</button>
+          </div>
+
+          <div>
+            <img src="/hero-image.png" alt="Hero Image"></img>
+          </div>
+
+        </div>
+    )
+}
